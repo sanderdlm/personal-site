6a8ec014e20121737f0448f8533e72d12147d159
Chet <sanderdlm69@gmail.com>
2017-04-03 18:51:24 +0200

    Initial commit

---
+++ 404.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sander De la Marche</title>
+	<meta charset="utf-8" />
+	<meta name="description" content="Sander De la Marche | Web Developer uit Heusden | Front-end & back-end">
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<link href="css/style.min.css" rel="stylesheet"/>
+	<link rel="shortcut icon" href="favicon.ico" />
+</head>
+<body>
+	<header>
+		<span>/*
+ *       Sander
+ *            De la Marche
+ */
+</span>
+	</header>
+	<section class='404'>
+		<h2><span class='classname'>.404</span> {</h2>
+		<ul>
+			<li><a href='/'>#back</a></li>
+		</ul>
+		<span>}</span>
+	</section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sander De la Marche</title>
+	<meta charset="utf-8" />
+	<meta name="description" content="Sander De la Marche | Web Developer">
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<link href="css/style.css" rel="stylesheet"/>
+	<link rel="shortcut icon" href="favicon.ico" />
+</head>
+<body>
+	<div class='wrap'>
+		<header>
+			<span class='comment'>/*
+ *       Sander
+ *            De la Marche
+ */</span>
+		</header>
+		<section class='bio'>
+			<h2><span class='classname'>.bio</span><span class='bracket'>{</span></h2>
+			<ul>
+				<li>age: 24;</li>
+				<li>interests: web, php, python, js, unix, running, hiking;</li>
+				<li>job: developer @ <a href="http://netcomsolutions.be/">Netcom Solutions</a></li>
+			</ul>
+			<span class='bracket closing'>}</span>
+		</section>
+		<section class='work'>
+			<h2><span class='classname'>.work</span><span class='bracket'>{</span></h2>
+			<ul>
+				<li>
+					<a href='http://www.joe-english.be' target='_blank'>joe-english.be</a>
+					<span><b>2015</b> - HTML / CSS / JS / Kirby / PHP / Photoshop;</span>
+				</li>
+				<li>
+					<a href='https://www.leaguecoaching.gg/' target='_blank'>leaguecoaching.gg</a>
+					<span><b>2013</b> - HTML / CSS;</span>
+				</li>
+				<li>
+					<a href='https://github.com/dreadnip' target='_blank'>> more on my github</a>
+				</li>
+			</ul>
+			<span class='bracket closing'>}</span>
+		</section>
+		<section class='contact'>
+			<h2><span class='classname'>.contact</span><span class='bracket'>{</span></h2>
+			<ul>
+				<li>mail: inbox at sanderdlm.be > <a href='Sander%20De%20la%20Marche%20inbox@sanderdlm.be%20(0x5244A7CA)%20pub.asc'>[PGP key]</a>;</li>
+			</ul>
+			<span class='bracket closing'>}</span>
+		</section>
+	</div>
+</body>
+</html>
